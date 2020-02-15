bb31a2cc5ecdf90368a1ce085e7771cfaa476417
floppp <altraciano@outlook.com>
2020-02-15 15:32:13 +0100

    memoization

--- src/types_and_functions.py
+++ src/types_and_functions.py
@@ -1,2 +1,66 @@
+from functools import reduce
+from typing import Callable, TypeVar
+from abc import ABCMeta
 
-lkasdf
+T = TypeVar('T')
+U = TypeVar('U')
+
+
+factorial: Callable[[int], int] = lambda x: x * \
+    factorial(x - 1) if x > 2 else x
+
+fact: Callable[[int], int] = lambda x: reduce(
+    lambda a, b: a * b, (i for i in range(1, x + 1)))
+
+
+def absurd(a: None) -> T:
+    pass
+
+
+f44: Callable[[], int] = lambda: 44
+
+
+f_int: Callable[[T], None] = lambda _: None
+
+
+def f_int_(x: T):
+    pass
+
+
+'''
+Two-element set
+'''
+
+
+class Bool(ABCMeta):
+    def __init__(self):
+        pass
+
+
+class True_(Bool):
+    def __init__(self):
+        super().__init__()
+
+
+class False_(Bool):
+    def __init__(self):
+        super().__init__()
+
+
+''' CHALLENGES '''
+# Memoization
+
+
+def memoization(fn: Callable[[T], U]):
+    __cache: dict = {}
+
+    def inner(*args: T):
+        if args not in __cache:
+            print('nuevos valores')
+            __cache[args] = fn(*args)
+        else:
+            print('valores ya cacheados')
+
+        return __cache[args]
+
+    return inner
--- tests/test_2_types_and_functions.py
+++ tests/test_2_types_and_functions.py
@@ -1,10 +1,20 @@
 import unittest
+from src.types_and_functions import factorial, fact
 
 
 class TestTypesFuncionts(unittest.TestCase):
 
-    def test(self):
-        pass
+    def test_factorial(self):
+        self.assertEqual(factorial(5), 120)
+        self.assertNotEqual(factorial(5), 121)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(2), 2)
+
+    def test_factorial_empty(self):
+        self.assertEqual(fact(1), 1)
+        self.assertEqual(fact(2), 2)
+        self.assertEqual(fact(5), 120)
+        self.assertNotEqual(fact(5), 121)
 
 
 if __name__ == '__main__':
